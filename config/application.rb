24ca25f5453e3b3d980ca8c26b733835d6a5b672
AlexTogar <smirnov.togar@ya.ru>
2018-09-03 22:40:22 +0300

    fix_config_timezone

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module FinSys
     config.action_view.field_error_proc = proc { |html_tag, _instance|
       html_tag
     }
+
+    config.time_zone = "Europe/Moscow"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
