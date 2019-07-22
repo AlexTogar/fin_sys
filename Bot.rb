bec5cf8e51f5e2d168c8b76d61866f1292ab110b
AlexTogar <smirnov.togar@ya.ru>
2019-07-22 18:11:04 +0300

    fix_3_balance

--- Bot.rb
+++ Bot.rb
@@ -1,10 +1,10 @@
 
   require_relative "Calc_query"
   require_relative "Telegram"
-#   require_relative "app/helpers/base_helper.rb"
+  require_relative "app/helpers/base_helper.rb"
   include Calculate
   include My_telegram
-#   include BaseHelper
+  include BaseHelper
 
   require_relative "app/models/reason"
   require_relative "app/models/transaction"
